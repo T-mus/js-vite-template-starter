a916f3e7fde91db3d2f00e7403d936f2d77e0da5
Raymond <adasta355@gmail.com>
2023-11-01 20:43:28 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import Inspect from 'vite-plugin-inspect'
+import injectHTML from 'vite-plugin-html-inject'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+
+export default defineConfig(() => {
+    return {
+        plugins: [
+            Inspect(),
+            injectHTML(),
+            ViteImageOptimizer({
+                png: {
+                    quality: 70,
+                },
+                jpeg: {
+                    quality: 70,
+                },
+                jpg: {
+                    quality: 70,
+                },
+            }),
+        ],
+        resolve: {
+            alias: {
+                // 'symbol': resolve(__dirname, './src/folder...')
+            },
+        },
+        build: {
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, 'index.html'),
+                    test: resolve(__dirname, 'pages/test.html'),
+                },
+            },
+        },
+    }
+})
